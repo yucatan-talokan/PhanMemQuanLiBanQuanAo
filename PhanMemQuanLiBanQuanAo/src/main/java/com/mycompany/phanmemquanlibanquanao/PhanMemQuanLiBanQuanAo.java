6a07c3f1dd7bd5e9ed1e6342066d46824373ed92
yucatan-talokan <cuongdmph22316@fpt.edu.vn>
2023-03-12 23:49:37 +0700

    cuong

---
+++ PhanMemQuanLiBanQuanAo/src/main/java/com/mycompany/phanmemquanlibanquanao/PhanMemQuanLiBanQuanAo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.phanmemquanlibanquanao;
+
+/**
+ *
+ * @author OSC
+ */
+public class PhanMemQuanLiBanQuanAo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
